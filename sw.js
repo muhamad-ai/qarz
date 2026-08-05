d9978c10d567378e68d931c0ebe587b0be6e8163
muhamad-ai <muhamad.j.slaman@gmail.com>
2026-08-05 10:57:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker — کاشکردنی ڕووکار بۆ کارکردنی خێراتر و PWA */
-const CACHE = "qarz-online-v9";
+const CACHE = "qarz-online-v10";
 const ASSETS = [
   "./index.html",
   "./app.html",
